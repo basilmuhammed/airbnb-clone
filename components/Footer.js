c75aa8b2a01da7a933af95cb6eb89fc0ce4a18c4
Basil Muhamemd <basilmuhd@outlook.com>
2021-08-15 01:18:53 +0530

    home page design with some api calling

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+const Footer = () => {
+  return (
+    <div className="grid grid-col-1 md:grid-col-4 lg:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">About</h5>
+        <p>How Airbnb workd</p>
+        <p>Newsroom</p>
+        <p>Invesotrs</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Community</h5>
+        <p>How Airbnb workd</p>
+        <p>Newsroom</p>
+        <p>Invesotrs</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Host</h5>
+        <p>How Airbnb workd</p>
+        <p>Newsroom</p>
+        <p>Invesotrs</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">Support</h5>
+        <p>How Airbnb workd</p>
+        <p>Newsroom</p>
+        <p>Invesotrs</p>
+        <p>Airbnb plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: [
+      "cdn.worldvectorlogo.com",
+      "images.unsplash.com",
+      "links.papareact.com",
+    ],
+  },
+};
